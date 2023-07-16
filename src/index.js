46c2d057292f420c2e85095e0a1e2b051c71eee7
huyxoann <huytn.21it@vku.udn.vn>
2023-07-16 07:29:34 +0700

    Xong trang chu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import $ from "jquery";
 import { Carousel } from "bootstrap";
 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars = document.querySelectorAll(".stars i");
+ // Loop through the "stars" NodeList
+ stars.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from 'firebase/app'
