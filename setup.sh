35b107e97220ea282e6fc000b405271cf3d484c0
huyxoann <huytn.21it@vku.udn.vn>
2023-08-15 16:01:25 +0700

    Add set up file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the project
+git clone https://github.com/huyxoann/camping_trip
+
+# Move into the project directory
+cd camping_trip
+
+# Install npm dependencies
+npm install
